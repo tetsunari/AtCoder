42e0df4ec042d693fcf8ba074487886cc8440830
tetsunari <mtst.tty556@gmail.com>
2024-07-05 17:26:30 +0900

    Welcome to AtCoder

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello, World");
+import * as fs from 'fs';
+
+const Main = (input: string) => {
+    const data = input;
+
+    const lines = data.split('\n');
+
+    const a = parseInt(lines[0]);
+    const [b, c] = lines[1].split(' ').map(Number);
+    const s = lines[2];
+
+    console.log(`${a + b + c} ${s}`);
+}
+
+Main(fs.readFileSync('/app/src/index.txt', 'utf-8'));
