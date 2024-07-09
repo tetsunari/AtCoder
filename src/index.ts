ac98a8b230122e7ebd7071385f0dbc0c58a59974
tetsunari <mtst.tty556@gmail.com>
2024-07-09 13:39:52 +0900

    return void main function

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs';
 
-const Main = (input: string) => {
+const Main = (input: string): void => {
     const data = input;
     const lines = data.split('\n');
     const a = parseInt(lines[0]);
