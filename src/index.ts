f7bb50bfce4dea1280e14a846bcd88195dc15ff7
tetsunari <mtst.tty556@gmail.com>
2024-07-09 13:33:50 +0900

    coins

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,21 @@ import * as fs from 'fs';
 const Main = (input: string) => {
     const data = input;
     const lines = data.split('\n');
-    const n = parseInt(lines[0]);
-    const a = lines[1].split(' ').map(Number);
+    const a = parseInt(lines[0]);
+    const b = parseInt(lines[1]);
+    const c = parseInt(lines[2]);
+    const x = parseInt(lines[3]);
 
     let count = 0;
-
-    while (a.every(x => x % 2 === 0)) {
-        for (let i = 0; i < n; i++) {
-            a[i] /= 2;
+    for (let i = 0; i < a + 1; i++) {
+        for (let j = 0; j < b + 1; j++) {
+            for (let k = 0; k < c + 1; k++) {
+                if (500 * i + 100 * j + 50 * k === x) {
+                    count++;
+                }
+            }
         }
-        count++;
     }
-
     console.log(count);
 }
 
