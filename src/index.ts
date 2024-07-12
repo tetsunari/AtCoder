3295802f6f0b122b691fcd2f51b85b8aacca06ef
tetsunari <mtst.tty556@gmail.com>
2024-07-12 13:21:51 +0900

    Card Game for Two

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
 import * as fs from 'fs';
 
 const Main = (input: string): void => {
-    const data = input;
-    const lines = data.split(' ').map(Number);
-    const [n, a, b] = lines;
+    const lines = input.trim().split('\n');
+    const n = Number(lines[0]);
+    const a = lines[1].split(' ').map(Number);
 
-    let sum = 0;
-    for (let i = 1; i <= n; i++) {
-        const num = i.toString().split('').map(Number).reduce((c, d) => c + d, 0);
-        if (num >= a && num <= b) {
-            sum += i;
+    a.sort((a, b) => b - a);
+    let sum1 = 0;
+    let sum2 = 0;
+    for (let i = 0; i < n; i++) {
+        if (i % 2 === 0) {
+            sum1 += a[i];
+        } else {
+            sum2 += a[i];
         }
     }
-
-    console.log(sum);
+    console.log(sum1 - sum2);
 }
 
 Main(fs.readFileSync('/app/src/index.txt', 'utf-8'));
