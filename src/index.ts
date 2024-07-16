2e96535e2547159b0c62bcac1f3911be4a1acca0
tetsunari <mtst.tty556@gmail.com>
2024-07-16 15:22:59 +0900

    Kagami Mochi

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,11 @@ import * as fs from 'fs';
 
 const Main = (input: string): void => {
     const lines = input.trim().split('\n');
-    const n = Number(lines[0]);
-    const a = lines[1].split(' ').map(Number);
+    const nums = lines.slice(1).map(Number);
 
-    a.sort((a, b) => b - a);
-    let sum1 = 0;
-    let sum2 = 0;
-    for (let i = 0; i < n; i++) {
-        if (i % 2 === 0) {
-            sum1 += a[i];
-        } else {
-            sum2 += a[i];
-        }
-    }
-    console.log(sum1 - sum2);
+    const unique = [...new Set(nums)];
+
+    console.log(unique.length);
 }
 
 Main(fs.readFileSync('/app/src/index.txt', 'utf-8'));
