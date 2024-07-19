18b6942a833729e70a8fb659f40565aa4cb9d4c2
tetsunari <mtst.tty556@gmail.com>
2024-07-19 11:28:58 +0900

    traveling

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,32 @@
 import * as fs from 'fs';
 
-const findBillCombination = (N: number, Y: number): [number, number, number] => {
-    for (let i = 0; i <= N; i++) {
-        for (let j = 0; j <= N - i; j++) {
-            const k = N - i - j;
-            if (i * 10000 + j * 5000 + k * 1000 === Y) {
-                return [i, j, k];
-            }
+const isTravelPlan = (N: number, plan: number[][]): boolean => {
+    let t_prev = 0;
+    let x_prev = 0;
+    let y_prev = 0;
+
+    for (const[t, x, y] of plan) {
+        const timeDiff = t - t_prev;
+        const distance = Math.abs(x - x_prev) + Math.abs(y - y_prev);
+
+        if (timeDiff < distance || (timeDiff - distance) % 2 !== 0) {
+            return false;
         }
+
+        t_prev = t;
+        x_prev = x;
+        y_prev = y;
     }
-    return [-1, -1, -1];
-}
+    return true;
+};
 
 const Main = (input: string): void => {
-    const [N, Y] = input.split('\n')[0].split(' ').map(Number);
-    const [a, b, c] = findBillCombination(N, Y);
-    console.log(`${a} ${b} ${c}`);
+    const lines = input.trim().split('\n');
+    const N = parseInt(lines[0]);
+    const S = lines.splice(1).map(line => line.split(' ').map(Number));
+
+    const ret = isTravelPlan(N, S) ? 'Yes' : 'No';
+    console.log(ret);
 }
 
 Main(fs.readFileSync('/app/src/index.txt', 'utf-8'));
