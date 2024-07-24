eef1c189b9486f7223687f849e94f804b04abfac
tetsunari <mtst.tty556@gmail.com>
2024-07-24 13:15:22 +0900

    Sum of 3 Integers

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,15 @@
 import * as fs from 'fs';
 
-class FruitBasket<T> {
-    private apple: number;
-    private mandarin: T;
-
-    constructor(mandarin: T) {
-        this.apple = 5;
-        this.mandarin = mandarin;
-    }
-
-    getTotalFruit(): number {
-        return this.apple + Number(this.mandarin);
-    }
-}
+const sum = (A: number[]): number => {
+    return A.reduce((prev,curr,index,array) => {
+        return prev + curr;
+    });
+};
 
 const Main = (input: string): void => {
     const lines = input.trim().split('\n');
-    const N = Number(lines[0]);
-    const basket = new FruitBasket<number>(N);
-    console.log(basket.getTotalFruit());
+    const A = lines[0].split(' ').map(Number);
+    console.log(sum(A));
 }
 
 Main(fs.readFileSync('/app/src/index.txt', 'utf-8'));
