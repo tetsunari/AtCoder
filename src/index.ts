75eebcd803378c42181a2a79369ddf0f11bbef0c
tetsunari <mtst.tty556@gmail.com>
2024-07-24 13:47:51 +0900

    Modulo 100

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import * as fs from 'fs';
 
-const sum = (A: number[]): number => {
+const mod = (A: number[]): number => {
     return A.reduce((prev,curr,index,array) => {
-        return prev + curr;
+        return (prev + curr) % 100;
     });
 };
 
 const Main = (input: string): void => {
     const lines = input.trim().split('\n');
-    const A = lines[0].split(' ').map(Number);
-    console.log(sum(A));
+    const A = lines[1].split(' ').map(Number);
+    console.log(mod(A));
 }
 
 Main(fs.readFileSync('/app/src/index.txt', 'utf-8'));
