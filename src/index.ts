5d91b4b94df81d6699c0328dffcf0627e4422754
tetsunari <mtst.tty556@gmail.com>
2024-07-26 13:51:23 +0900

    Brute Force 1

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,22 @@
 import * as fs from 'fs';
 
-class CountMultiples {
-    private N: number;
-    private X: number;
-    private Y: number;
-
-    constructor(data: number[]) {
-        [this.N, this.X, this.Y] = data;
+const Brute = ([N, S]: number[]): number => {
+    let count = 0;
+    for (let i = 1; i <= N; ++i) {
+        for (let j = 1; j <= N; ++j) {
+            if (i + j > S) {
+                break;
+            }
+            ++count;
+        }
     }
-
-    private gcd(a: number, b: number): number {
-        return b === 0 ? a : this.gcd(b, a % b);
-    }
-
-    private lcm(a: number, b: number): number {
-        return a * (b / this.gcd(a, b));
-    }
-
-    exec(): number {
-        const countX = Math.floor(this.N / this.X);
-        const countY = Math.floor(this.N / this.Y);
-        const lcm = this.lcm(this.X, this.Y);
-        const countXY = Math.floor(this.N / lcm);
-        return countX + countY - countXY;
-    }
+    return count;
 }
 
 const Main = (input: string): void => {
     const lines = input.trim().split('\n');
     const data = lines[0].split(' ').map(Number);
-    const math = new CountMultiples(data);
-    console.log(math.exec());
+    console.log(Brute(data));
 }
 
 Main(fs.readFileSync('/app/src/index.txt', 'utf-8'));
