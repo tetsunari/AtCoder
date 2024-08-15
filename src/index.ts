2f88a2d039357e436f701d9e062f18145a909fa6
tetsunari <mtst.tty556@gmail.com>
2024-08-15 13:48:40 +0900

    Combination Easy

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,22 @@
 import * as fs from 'fs';
 
-const chooseWay = (A: number[]): number => {
-    const dp: number[][] = Array.from({length: 6}, () => Array(1001).fill(0));
-    console.log(dp);
-    dp[0][0] = 1;
-
-    for (let card of A) {
-        for (let i = 5; i >= 1; i--) {
-            for (let j = 1000; j >= card; j--) {
-                dp[i][j] += dp[i - 1][j - card];
-            }
-        }
+function times(x: number): number
+{
+    let result = 1;
+    for (let i = 1; i <= x; i++) {
+        result *= i;
     }
-    return dp[5][1000];
-};
+    return result;
+}
+
+const combination = (n: number, r: number): number => {
+    return (times(n) / (times(r) * times(n - r)) );
+}
 
 const Main = (input: string): void => {
     const lines = input.trim().split('\n');
-    // const N = parseInt(lines[0]);
-    const A = lines[1].split(' ').map(Number);
-    console.log(chooseWay(A));
+    const[n, r] = lines[0].split(' ').map(Number);
+    console.log(combination(n, r));
 }
 
 Main(fs.readFileSync('/app/src/index.txt', 'utf-8'));
