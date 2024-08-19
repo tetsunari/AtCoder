5690d408cc58437cd1ffb8db6cfb8e5f30554509
tetsunari <mtst.tty556@gmail.com>
2024-08-19 13:12:46 +0900

    Dice Expectation

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,15 @@
 import * as fs from 'fs';
 
-const countPairs = (N: number, A: number[]): number => {
-    const counts = new Map<number, number>();
-
-    for (let i = 0; i < N; i++) {
-        const value = A[i];
-        if (counts.has(value)) {
-            counts.set(value, counts.get(value)! + 1);
-        } else {
-            counts.set(value, 1);
-        }
-    }
-
-    let pairs = 0;
-    for (const [value, count] of counts.entries()) {
-        const complement = 100000 - value;
-        if (counts.has(complement)) {
-            if (value === complement) {
-                pairs += (count * (count - 1)) / 2;
-            } else if (value < complement) {
-                pairs += count * counts.get(complement)!;
-            }
-        }
-    }
-    return pairs;
+const expectation = (N: number, B: number[], R: number[]): number => {
+    return B.reduce((pre, curr) => pre + curr, 0) / N + R.reduce((pre, curr) => pre + curr, 0) / N;
 }
 
 const Main = (input: string): void => {
     const lines = input.trim().split('\n');
     const N = Number(lines[0]);
-    const A = lines[1].split(' ').map(Number);
-    console.log(countPairs(N, A));
+    const B = lines[1].split(' ').map(Number);
+    const R = lines[2].split(' ').map(Number);
+    console.log(expectation(N, B, R).toFixed(6));
 }
 
 Main(fs.readFileSync('/app/src/index.txt', 'utf-8'));
