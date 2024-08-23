8bd017d1eb4583b687929a8af88fb2ed4ca01596
tetsunari <mtst.tty556@gmail.com>
2024-08-23 12:22:07 +0900

    Answer Exam Randomly

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,26 @@
 import * as fs from 'fs';
 
-const expectation = (N: number, B: number[], R: number[]): number => {
-    return B.reduce((pre, curr) => pre + curr, 0) / N + R.reduce((pre, curr) => pre + curr, 0) / N;
-}
+interface Problem {
+    P: number,
+    Q: number,
+};
+
+const calcExpectedScore = (problems: Problem[]): number => {
+    let expectedScore = 0;
+    for (const problem of problems) {
+        expectedScore += problem.Q / problem.P;
+    }
+    return expectedScore;
+};
 
 const Main = (input: string): void => {
     const lines = input.trim().split('\n');
     const N = Number(lines[0]);
-    const B = lines[1].split(' ').map(Number);
-    const R = lines[2].split(' ').map(Number);
-    console.log(expectation(N, B, R).toFixed(6));
+    const problems: Problem[] = lines.slice(1, N + 1).map(line => {
+        const [P, Q] = line.split(' ').map(Number);
+        return {P, Q};
+    });
+    console.log(calcExpectedScore(problems).toFixed(6));
 }
 
 Main(fs.readFileSync('/app/src/index.txt', 'utf-8'));
