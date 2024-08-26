d89de1420c4594f075c14e5767a3e26b2d8f7bfb
tetsunari <mtst.tty556@gmail.com>
2024-08-26 14:21:54 +0900

    Jiro's Vacation

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,17 @@
 import * as fs from 'fs';
 
-interface Problem {
-    P: number,
-    Q: number,
-};
-
-const calcExpectedScore = (problems: Problem[]): number => {
-    let expectedScore = 0;
-    for (const problem of problems) {
-        expectedScore += problem.Q / problem.P;
-    }
-    return expectedScore;
+const calcExpectedScore = <T extends number>(A: T[], B: T[]): T => {
+    let expectedScoreA = A.reduce((prev: number, curr: number) => prev + (curr / 3), 0 as T);
+    let expectedScoreB = B.reduce((prev: number, curr: number) => prev + (curr * 2 / 3), 0 as T);
+    return (expectedScoreA + expectedScoreB) as T;
 };
 
 const Main = (input: string): void => {
     const lines = input.trim().split('\n');
-    const N = Number(lines[0]);
-    const problems: Problem[] = lines.slice(1, N + 1).map(line => {
-        const [P, Q] = line.split(' ').map(Number);
-        return {P, Q};
-    });
-    console.log(calcExpectedScore(problems).toFixed(6));
+    // const N = Number(lines[0]);
+    const A = lines[1].split(' ').map(Number);
+    const B = lines[2].split(' ').map(Number);
+    console.log(calcExpectedScore(A, B).toFixed(3));
 }
 
 Main(fs.readFileSync('/app/src/index.txt', 'utf-8'));
