c50ad74ba5e32662c589f1df727d16cc0bd9633c
tetsunari <mtst.tty556@gmail.com>
2024-08-27 17:13:55 +0900

    Sorting

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,39 @@
 import * as fs from 'fs';
 
-const calcExpectedScore = (N: number): number => {
-    let expectedScore = 0;
-    for (let i = 1; i <= N; i++) {
-        expectedScore += N / i;
+const mergeSort = (A: number[]): number[] => {
+    if (A.length <= 1) {
+        return A;
     }
-    return expectedScore;
-};
+
+    const middle = Math.floor(A.length / 2);
+    const left = A.slice(0, middle);
+    const right = A.slice(middle);
+
+    return mergeList(mergeSort(left), mergeSort(right));
+}
+
+const mergeList = (left: number[], right: number[]): number[] => {
+    let result: number[] = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            result.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            result.push(right[rightIndex]);
+            rightIndex++;
+        }
+    }
+
+    return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
 
 const Main = (input: string): void => {
     const lines = input.trim().split('\n');
-    const N = Number(lines[0]);
-    console.log(calcExpectedScore(N).toFixed(6));
+    const A = lines[1].split(' ').map(Number);
+    console.log(mergeSort(A).join(' '));
 }
 
 Main(fs.readFileSync('/app/src/index.txt', 'utf-8'));
