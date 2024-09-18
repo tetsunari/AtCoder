d0eb112b9cedfa7c73cf61e754c28a216c972d64
tetsunari <mtst.tty556@gmail.com>
2024-09-18 12:08:30 +0900

    Binary Search

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,14 @@
 import * as fs from "fs";
 
-const Stady = (N: number, A: number[]): number => {
-  if (N === 0) return 0;
-  if (N === 1) return A[0];
-
-  let dp: number[] = new Array(N).fill(0);
-  dp[0] = A[0];
-  dp[1] = Math.max(A[0], A[1]);
-
-  for (let i = 2; i < N; i++) {
-    dp[i] = Math.max(dp[i - 1], dp[i - 2] + A[i]);
-  }
-  return dp[N - 1];
+const findX = (X: number, A: number[]): string => {
+  return A.includes(X) ? "Yes" : "No";
 };
 
 const Main = (input: string): void => {
   const lines = input.trim().split("\n");
-  const N = parseInt(lines[0]);
+  const [N, X]: number[] = lines[0].split(" ").map(Number);
   const A = lines[1].split(" ").map(Number);
-  console.log(Stady(N, A));
+  console.log(findX(X, A));
 };
 
 Main(fs.readFileSync("/app/src/index.txt", "utf-8"));
